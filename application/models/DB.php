c203f287e3cf40077c91ca041b5f0c96dc3e1ee4
Eric Murphy <emurphy@fullsail.edu>
2013-05-20 02:46:35 -0400

    Api testing

---
+++ application/models/DB.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ericmurphy
+ * Date: 5/20/13
+ * Time: 2:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class DB
+{
+    public function __construct(){
+        //Try creating a new PDO connection to DB, else var_dump any errors.
+        try
+        {
+            $dsn = "mysql:host=127.0.0.1;port=8889;dbname=mdd";
+            $user = "root";
+            $pass = "root";
+
+            $this->db = new \PDO($dsn, $user, $pass);
+            // do something that can go wrong
+        }
+        catch (PDOException $e)
+        {
+            //throw new Exception( 'Something really gone wrong', 0, $e);
+            var_dump($e);
+        }
+    }//__construct
+}
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -63,26 +63,82 @@
 		-webkit-box-shadow: 0 0 8px #D0D0D0;
 	}
 	</style>
+
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript">
+        //<![CDATA[
+
+        var customIcons = {
+            truckstop: {
+                icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png',
+                shadow: 'http://labs.google.com/ridefinder/images/mm_20_shadow.png'
+            }
+        };
+
+        function load() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                center: new google.maps.LatLng(41.03427200, -77.51681500),
+                zoom: 15,
+                mapTypeId: 'roadmap'
+            });
+            var infoWindow = new google.maps.InfoWindow;
+
+            // Change this depending on the name of your PHP file
+            downloadUrl("applications/models/genxml.php", function(data) {
+                var xml = data.responseXML;
+                var markers = xml.documentElement.getElementsByTagName("marker");
+                for (var i = 0; i < markers.length; i++) {
+                    var name = markers[i].getAttribute("name");
+                    var address = markers[i].getAttribute("address");
+                    var type = markers[i].getAttribute("type");
+                    var point = new google.maps.LatLng(
+                        parseFloat(markers[i].getAttribute("lat")),
+                        parseFloat(markers[i].getAttribute("lng")));
+                    var html = "<b>" + name + "</b> <br/>" + address;
+                    var icon = customIcons[type] || {};
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: point,
+                        icon: icon.icon,
+                        shadow: icon.shadow
+                    });
+                    bindInfoWindow(marker, map, infoWindow, html);
+                }
+            });
+        }
+
+        function bindInfoWindow(marker, map, infoWindow, html) {
+            google.maps.event.addListener(marker, 'click', function() {
+                infoWindow.setContent(html);
+                infoWindow.open(map, marker);
+            });
+        }
+
+        function downloadUrl(url, callback) {
+            var request = window.ActiveXObject ?
+                new ActiveXObject('Microsoft.XMLHTTP') :
+                new XMLHttpRequest;
+
+            request.onreadystatechange = function() {
+                if (request.readyState == 4) {
+                    request.onreadystatechange = doNothing;
+                    callback(request, request.status);
+                }
+            };
+
+            request.open('GET', url, true);
+            request.send(null);
+        }
+
+        function doNothing() {}
+
+        //]]>
+
+    </script>
+
 </head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-</div>
-
+<body onload="load()">
+    <div id="map" style="width: 500px; height: 300px"></div>
 </body>
+
 </html>
